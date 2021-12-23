30ebbc491324de40703ca154fce61165dcc0dc3b
frodod <71426230+frodod@users.noreply.github.com>
2021-12-23 13:15:11 +0200

    Lab 1-8

---
+++ LAB_01_Lipskiy/LAB_01_Lipskiy/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace LAB_01_Olshewski
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_pass);
+            this.groupBox1.Controls.Add(this.textBox_login);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Авторизація";
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(100, 19);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(94, 20);
+            this.textBox_login.TabIndex = 0;
+            // 
+            // textBox_pass
+            // 
+            this.textBox_pass.Location = new System.Drawing.Point(100, 49);
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.Size = new System.Drawing.Size(94, 20);
+            this.textBox_pass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логін";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(218, 12);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(48, 100);
+            this.button_login.TabIndex = 1;
+            this.button_login.Text = "Log in";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Menu;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(95, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "               ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Menu;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(95, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 26);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "               ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 120);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_pass;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LAB_01_Lipskiy/LAB_01_Lipskiy/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_01_Olshewski
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_click(object sender, EventArgs e)
+        {
+            if (textBox_login.Text == "NumbDigger" && textBox_pass.Text == "milkies")
+            {
+                label3.BackColor = Color.Wheat;
+                label4.BackColor = Color.Wheat;
+                MessageBox.Show("Hello");
+            }
+            else
+            {
+                if (textBox_login.Text == "NumbDigger")
+                {
+                    label3.BackColor = Color.Green;
+                    label4.BackColor = Color.Red;
+                }
+                if(textBox_pass.Text == "milkies")
+                {
+                    label3.BackColor = Color.Red;
+                    label4.BackColor = Color.Green;
+                }
+                if (!(textBox_login.Text == "NumbDigger") && !(textBox_pass.Text == "milkies"))
+                {
+                    label3.BackColor = Color.Red;
+                    label4.BackColor = Color.Red;
+                }                
+                MessageBox.Show("Error! Fart Smella!");
+            }
+        }
+    }
+}
